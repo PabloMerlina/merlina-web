3047f40060ec0cacd4bb6fa923ee832fe5b1e91f
Pablo Sepulveda <pablo@merlina.ai>
2026-04-28 15:18:00 -0400

    feat: add Meta Pixel tracking (ID: 1069839642886186)
    - Add Meta Pixel base code in app/layout.tsx <head>
    - Track PageView event on all pages
    - Include noscript fallback for users without JavaScript
    - Use next/script with afterInteractive strategy for optimal loading
    - Enable conversion tracking and audience building

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { GTM } from "@/components/GTM";
 import "./globals.css";
 
@@ -55,6 +56,34 @@ export default function RootLayout({
             }),
           }}
         />
+        {/* Meta Pixel */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1069839642886186');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1069839642886186&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
       </head>
       <body className="antialiased">{children}</body>
     </html>
